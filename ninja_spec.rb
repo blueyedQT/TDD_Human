d7ace68f3c4c05a754ae5e5be0d1ad4f33fb7447
Katrina <blueyed_qt@hotmail.com>
2015-11-07 19:41:00 -0800

    Set up ninja

---
+++ ninja_spec.rb
@@ -0,0 +1,13 @@
+require_relative "ninja"
+
+RSpec.describe Ninja do
+	if "has an ancestor chain that includes Human" do
+		# your code here
+	end
+	it "has a steal method that increases its stealth by 10" do
+		# your code here
+	end
+	it "has a default stealth of 175" do
+		# your code here
+	end
+end
