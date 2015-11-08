f2979e7e7b5445570a51121882ba9c6875174a58
Katrina <blueyed_qt@hotmail.com>
2015-11-07 19:48:10 -0800

    Added tests and method for ninja

--- ninja.rb
+++ ninja.rb
@@ -1,2 +1,11 @@
-class Ninja
+require_relative "human"
+
+class Ninja < Human
+	def initialize
+		@stealth = 175
+	end
+
+	def steal
+		@stealth += 10
+	end
 end
--- ninja_spec.rb
+++ ninja_spec.rb
@@ -1,13 +1,18 @@
 require_relative "ninja"
 
 RSpec.describe Ninja do
-	if "has an ancestor chain that includes Human" do
-		# your code here
+	it "has an ancestor chain that includes Human" do
+		human = Ninja.ancestors.include?(Human)
+		expect(human).to eq(true)
 	end
 	it "has a steal method that increases its stealth by 10" do
-		# your code here
+		ninja = Ninja.new
+		ninja.stealth = 0
+		ninja.steal
+		expect(ninja.stealth).to eq(10)
 	end
 	it "has a default stealth of 175" do
-		# your code here
+		ninja = Ninja.new
+		expect(ninja.stealth).to eq(175)
 	end
 end
