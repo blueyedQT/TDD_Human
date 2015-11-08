6f3e34ecce99acf461532eeaf95bbb7af55e6363
Katrina <blueyed_qt@hotmail.com>
2015-11-07 20:04:34 -0800

    Added tests and methods to samurai class

--- samuari_spec.rb
+++ samuari_spec.rb
@@ -1,5 +1,23 @@
 require_relative 'samurai'
 
 RSpec.describe Samurai do
-	# your code here
+	it "has an ancestor chain that includes Human" do 
+		human = Samurai.ancestors.include?(Human)
+		expect(human).to eq(true)
+	end
+
+	it "has a default intelligence of 50 and a strength of 40" do 
+		samurai = Samurai.new
+		expect(samurai.intelligence).to eq(50)
+		expect(samurai.strength).to eq(40)
+	end
+
+	it "has a method of adding strength exchange for intelligence" do
+		samurai = Samurai.new
+		samurai.strength = 30
+		samurai.intelligence = 50
+		samurai.addstrength
+		expect(samurai.strength).to eq(35)
+		expect(samurai.intelligence).to eq(45)
+	end
 end
--- samurai.rb
+++ samurai.rb
@@ -1,2 +1,13 @@
-class Samurai
+require_relative "human"
+
+class Samurai < Human
+	def initialize
+		@intelligence = 50
+		@strength = 40
+	end
+
+	def addstrength
+		@intelligence -= 5
+		@strength += 5
+	end
 end
