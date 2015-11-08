3586bb64abed39760589268fd969ef3e68707052
Katrina <blueyed_qt@hotmail.com>
2015-11-07 18:17:32 -0800

    Set up for Human 2

--- human_spec.rb
+++ human_spec.rb
@@ -2,7 +2,6 @@ require_relative "human"
 
 RSpec.describe Human do 
 	describe "initialization" do 
-		# before running any of the tests below ...
 		before do 
 			@human = Human.new
 		end
@@ -38,6 +37,24 @@ RSpec.describe Human do
 		it "has a getter and a setter for health" do 
 			@human.health = 0
 			expect(@human.health).to eq(0)
+		end
+	end
+	describe "behavior" do 
+		before do 
+			@human1 = Human.new
+			@human2 = Human.new
+		end
+		it "attacks another object if the object inherits from Human" do 
+			nothuman = Array.new
+			success1 = @human1.attack(@human2)
+			expect(success1).to eq(true)
+			success2 = @human1.attack(nothuman)
+			expect(success2).to eq(false)
+		end
+		it "decreases the object's health by 10" do 
+			@human2.health = 10
+			@human1.attack(@human2)
+			expect(@human2.health).to eq(0)
 		end
 	end
 end
